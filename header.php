4e4a167e7c9f2eaf621cac59d2db4e7bb5106858
abdulrahmanroston <119020009+abdulrahmanroston@users.noreply.github.com>
2023-11-17 00:59:08 -0800

    qq

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html> 
 
-<html> <?php language_attributes() ; ?>
+<html <?php language_attributes() ; ?> >
     <head>
-        <meta charset="<?php bloginfo('charset') ?>" >
+        <meta charset="<?php bloginfo('charset') ?>" />
         <title><?php bloginfo('name') ?></title >
-        <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" >
+        <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
         <?php wp-head(); ?>
     </head>
