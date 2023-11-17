992815ac16d47898cbbbb91834597ddd23c6766b
abdulrahmanroston <119020009+abdulrahmanroston@users.noreply.github.com>
2023-11-17 00:28:18 -0800

    add header

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="<?php bloginfo('charset') ?>" >
+        <title><?php bloginfo('name') ?></title>
+        <link rel="pingback" href="<?php bloginfo('pingback_url') ?>">
+        <?php wp-head(); ?>
+    </head>
